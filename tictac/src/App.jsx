9dd1162784f98f96d1e9d56bce6cca7a304e66e0
Muhammadali7866 <malikali8157@gmail.com>
2024-01-06 17:21:35 +0500

    fix

---
+++ tictac/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import TicTack from "./Components/TicTacToe/TIcTack";
+const App = () => {
+  return (
+    <div className="screen-xl ">
+      <TicTack />
+    </div>
+  );
+};
+
+export default App;
